85e63f634dc65d5d7ff44315ac3776a6e50c4848
Elias Perreira Miguel <eliasperreiramiguel@gmail.com>
2023-03-17 22:46:28 -0300

    atualizando

---
+++ main.js
@@ -0,0 +1,25 @@
+const forml =document.getElementById('formN')
+
+function validaForm(campvalido){
+  const numArray =campvalido.split(' ');
+  return numArray.length > camB.value.length;
+}
+forml.addEventListener('submit', function(e){
+  e.preventDefault();
+
+  let camA = document.getElementById('campoa')
+  let camB = document.getElementById('campob')
+
+  if ()
+/*let camA = document.getElementById('campoa')
+let camB = document.getElementById('campob')
+let res = document.getElementById('res')
+let campoA= Number(camA.value)
+let campoB= Number(camB.value)
+
+if (campoA > campoB ||campoB=='0'){
+  res.innerHTML=('[ERRO] campo A maior que o B')
+} else {
+  alert('Tudo ok')
+}*/
+})
