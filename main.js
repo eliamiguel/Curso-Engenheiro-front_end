2d2e1cf1dc4b7fb47b7f8dde961b348f070127c4
Elias Perreira Miguel <eliasperreiramiguel@gmail.com>
2023-03-20 21:36:32 -0300

    finalizado atividade

--- main.js
+++ main.js
@@ -1,9 +1,96 @@
-const form =document.getElementById('form')
+/*const form =document.getElementById('form')
 let input = document. querySelector('#numero1')
 let inputcampb = document.querySelector('#numero2')
-const mensse =document.querySelector('.res')
-console.log(input)
 
+console.log(input)*/
+const form =document.getElementById('form')
+const mensse =document.querySelector('.res')
+let input = document.getElementById('numero1')
+let inputcampb = document.getElementById('numero2')
+let validarForm = true
+
+form.addEventListener('submit', function(e){
+  e.preventDefault();
+  validar()
+
+  if(validarForm){
+    form.remove()
+    mensse.classList.remove('erroCampoa')}
+})
+
+function validar(){
+  validarForm =false
+  if (input.value.length > inputcampb.value.length){
+    inputcampb.classList.add('invalidB')
+    inputcampb.nextElementSibling.classList.remove('erroCampob')
+  } else {
+    validarForm =true
+      input.classList.remove('invalidA')
+      input.nextElementSibling.classList.add('erroCampoa')
+      
+      
+    } 
+
+    
+  
+}
+input.addEventListener('input', ()=>{
+  inputcampb.classList.remove('invalidB')
+    inputcampb.nextElementSibling.classList.add('erroCampob')
+input.classList.remove('invalidA')
+input.nextElementSibling.classList.add('erroCampoa')
+})
+
+
+
+/* input.classList.add('invalidA')
+    input.nextElementSibling.classList.remove('erroCampoa')
+    validarForm = true
+/*input.addEventListener('input', ()=>{
+  inputcampb.classList.remove('invalidB')
+    inputcampb.nextElementSibling.classList.add('erroCampob')
+input.classList.remove('invalidA')
+input.nextElementSibling.classList.add('erroCampoa')
+})*/
+
+
+/*
+ input.classList.remove('invalidA')
+    input.nextElementSibling.classList.add('erroCampoa')
+const form = document.getElementById("form");
+let formEvalido = false;
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  validaForm();
+});
+
+function validaForm() {
+  let campoA = document.getElementById("campo-a");
+  let campoB = document.getElementById("campo-b");
+  let mensagemSucesso = `Parabéns!!!  <b>${campoB.value}</b> é maior que o <b> ${campoA.value}</b>!!!`;
+  let mensagemErro = `Error!!! <b> ${campoB.value} </b> é menor que o <b> ${campoA.value}</b>!!!`;
+  let formEvalido = campoB.value > campoA.value;
+  if (formEvalido) {
+    let containerSucesso = document.querySelector(".mensagem-certa");
+    containerSucesso.innerHTML = mensagemSucesso;
+    containerSucesso.style.display = "block";
+
+    campoA.value = "";
+    campoB.value = "";
+  } else {
+    let containerErro = document.querySelector(".mensagem-erro");
+    containerErro.innerHTML = mensagemErro;
+    containerErro.style.display = "block";
+
+    campoA.value = "";
+    campoB.value = "";
+  }
+}
+
+function limparMensagem() {}
+*/
 
 /*form.addEventListener('submit', function(e){
   e.preventDefault();
