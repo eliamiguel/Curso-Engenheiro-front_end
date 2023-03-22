67085e789e5d29b69c8f0adaccecd7b427965e08
Elias Perreira Miguel <eliasperreiramiguel@gmail.com>
2023-03-21 22:41:58 -0300

    terminei a primeira parte

--- ex8.1/main.js
+++ ex8.1/main.js
@@ -1,12 +1,29 @@
-const form = document.getElementById('form-atividade');
+const form = document.getElementById("form-atividade");
+const imgAprovado = '<img src="images/aprovado.png" alt="emoji festejando" />'
+const imgRprovado = '<img src = "images/reprovado.png" alt="Emoji decepcionado"/>'
+
+let linhas = ' ';
 form.addEventListener('submit', function(e) {
   e.preventDefault(); 
 
 const inputNomeAtividade = document.getElementById('nome-atividade')
 const inputNotaAtividade = document.getElementById('nota-atividade')
+let linha = '<tr>'
+linha += `<td>${inputNomeAtividade.value} </td>`
+linha +=`<td>${inputNotaAtividade.value} </td>`
+linha +=`<td>${inputNotaAtividade.value >=7 ? imgAprovado : imgRprovado } </td>`
+linha +='</tr>'
 
-alert(`atividade: ${inputNomeAtividade.value.length} - nota: ${inputNotaAtividade.value.length}`);
+linhas += linha;
+
+const corpoTablela = document.querySelector('tbody')
+corpoTablela.innerHTML = linhas
+
+inputNomeAtividade.value=" "
+inputNotaAtividade.value =" "
+
 });
+
 
 
 
