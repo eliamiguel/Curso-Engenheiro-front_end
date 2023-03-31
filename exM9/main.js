b34c00854e368543dc850f086f6bc15d7674ccb6
Elias Perreira Miguel <eliasperreiramiguel@gmail.com>
2023-03-31 19:19:34 -0300

    11

---
+++ exM9/main.js
@@ -0,0 +1,13 @@
+
+  
+
+   const inputTarefa=document.getElementById('#nome').val()
+   const lista= [
+    $('<li></li>').appendTo(inputTarefa)
+   ]
+   const elementos=$('<ul><ul>')>appendTo(lista)
+   $('#Conteudo1').appendTo(elementos)
+    
+
+
+
