a243c53ee5c91353c2e70696f21b1cce5cb37c1b
Elias Perreira Miguel <eliasperreiramiguel@gmail.com>
2023-04-02 22:30:37 -0300

    salvando

---
+++ exM9/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+   const list=$('<li></li>')
+   $('form').on ('submit', function(e){
+      e.preventDefault();
+      const inputNomeTarefa= $('#nome').val()
+      
+      $(`<li id="i">${inputNomeTarefa}</li>`).appendTo(list)
+      $(list).appendTo('ul');
+      $('#nome').val('')
+      
+   })
+   $(list).click(function(){
+      $(list).addClass('itens')
+   })
+   
+})
+
