729e164664bd18fd85ab736ca6ae7f0db3dadbfe
Elias Perreira Miguel <eliasperreiramiguel@gmail.com>
2023-04-02 22:06:53 -0300

    elias

--- exM9/main.js
+++ exM9/main.js
@@ -1,13 +1,17 @@
-
-  
-
-   const inputTarefa=document.getElementById('#nome').val()
-   const lista= [
-    $('<li></li>').appendTo(inputTarefa)
-   ]
-   const elementos=$('<ul><ul>')>appendTo(lista)
-   $('#Conteudo1').appendTo(elementos)
-    
-
-
+$(document).ready(function(){
+   const list=$('<li></li>')
+   $('form').on ('submit', function(e){
+      e.preventDefault();
+      const inputNomeTarefa= $('#nome').val()
+      
+      $(`<li id="i">${inputNomeTarefa}</li>`).appendTo(list)
+      $(list).appendTo('ul');
+      $('#nome').val('')
+      
+   })
+   $(list).click(function(){
+      $(list).addClass('itens')
+   })
+   
+})
 
